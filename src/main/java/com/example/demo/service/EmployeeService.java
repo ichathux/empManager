c80195610fc74e2039393e38d27fed61b727bb07
ichathux <ichathux@gmail.com>
2022-11-27 15:50:01 +0530

    more on employee service and custom exception handle

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +21,18 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode (UUID.randomUUID ().toString ());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll ();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save (employee);
+    }
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findById (id).orElseThrow (() -> new UserNotFoundException ("user "+id+" not found"));
+    }
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteById (id);
     }
 }
