b536702a5f405cb778284e9defaada972ac42c7f
ichathux <ichathux@gmail.com>
2022-11-27 15:31:11 +0530

    created employee repo

---
+++ src/main/java/com/example/demo/repo/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repo;
+
+import com.example.demo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
